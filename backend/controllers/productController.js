fac8e6868f1e7d2337a71508fa2508c0d96fa2ba
Isurulak1 <isurulakshan0417@gmail.com>
2024-09-28 18:55:28 +0530

    Create User Authentication feature

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+
+
+// funtion for add product
+
+const addProduct = async (req, res) => {
+    
+}
+
+// funtion for lis product
+
+const listProduct = async (req, res) => {
+    
+}
+
+// funtion for remove product
+
+const removeProduct = async (req, res) => {
+    
+}
+
+// funtion for single product info
+
+const singleProduct = async (req, res) => {
+    
+}
+
+export { addProduct, listProduct, removeProduct, singleProduct }
